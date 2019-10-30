07de4b5c0775a0094767d5838c80ca206af3ebd0
Ruairi-osul <rorylikesmusic@gmail.com>
2019-10-30 15:49:36 -0400

    added to_black_white option

--- video_preprocessing/video_preprocess.py
+++ video_preprocessing/video_preprocess.py
@@ -4,6 +4,7 @@ Takes one or more input .json file containing the following fields:
     - output_name: the name of the output video file to be saved
     - output_bitrate: The bitrate of the output video file. 
         	          This corresponds to the quality of the output video
+    - to_black_white: Boolean of whether to convert to black and white or not
 
 It compresses, concatenates and saves the resulting video.
 
@@ -22,6 +23,7 @@ $ python video_preprocess.py somefolder/inputjson.json
 import sys
 import moviepy.editor as mp
 import json
+import moviepy.video.fx.all as vfx
 
 
 def json_to_dict(file_path):
@@ -37,7 +39,7 @@ def json_to_dict(file_path):
     return output
 
 
-def main(videos, output_name="output.mp4", output_bitrate="500k"):
+def main(videos, output_name="output.mp4", output_bitrate="500k", to_black_white=True):
     """
     Takes a list of video paths. It compresses them to a specified bitrate
     and concatenates them together and saving them to a file
@@ -47,17 +49,18 @@ def main(videos, output_name="output.mp4", output_bitrate="500k"):
         - output_name: the name of the output video file to be saved
         - output_bitrate: The bitrate of the output video file. 
                           This corresponds to the quality of the output video 
+        - to_black_white: Boolean of whether to convert to black and white or not
     """
     videos = list(map(lambda x: mp.VideoFileClip(x), videos))
     concattenated_video = mp.concatenate_videoclips(videos)
-    concattenated_video.write_videofile(output_name, bitrate=output_bitrate)
+
+    if to_black_white:
+        concattenated_video = concattenated_video.fx(vfx.blackwhite)
+
+        concattenated_video.write_videofile(output_name, bitrate=output_bitrate)
 
 
 if __name__ == "__main__":
     for input_json in sys.argv[1:]:
         config_options = json_to_dict(input_json)
-        main(
-            videos=config_options["videos"],
-            output_name=config_options["output_name"],
-            output_bitrate=config_options["output_bitrate"],
-        )
+        main(**config_options)
