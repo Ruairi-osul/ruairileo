61ec8e66a398a9267438763268c79615f3fb45f6
Ruairi-osul <rorylikesmusic@gmail.com>
2020-04-13 13:21:02 +0100

    leo freeze task

---
+++ freeze/readme.md
@@ -0,0 +1,16 @@
+# Goal: 
+
+Write a class that analyses freezes in a video.
+
+The desired behaviour of the class
+
+```{python}
+video_path = "my_video.avi"
+processor = FreezeProcessor()
+freezing_events = processor.process_data(video_path)
+```
+
+## Things you'll need to do
+
+1. Access the functions from ezTrack that. You can either copy and paste them into the file, or make them methods of FreezeProcessor.
+2. Write the process_data method of FreezeProcessor. You will use the functions from ezTrack to do this.
---
+++ freeze/todo.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# you can load in the functions from the freeze analysis package here, or write them in the class
+
+
+class FreezeProcessor:
+    @staticmethod
+    def some_eztrack_function(eztrack_arg1, etc):
+        """
+        You might want to include the eztrack functions in the class.
+        @staticmethod just means that the object it not referenced in the method call.
+        For our case it means you can use the functions the same way as normal functions.
+        To access these functions elsewhere in the class, call self.some_eztrack_function(eztrack_arg1="some_value").
+        """
+        # copy and paste here!
+        pass  # pass just means ignore this
+
+    def process_data(self, path: str) -> np.ndarray:
+        """
+        Your job is to write this method.
+
+        Takes a path of a video file and returns a numpy array of freezing times.
+        """
+        # your code here
+        pass
